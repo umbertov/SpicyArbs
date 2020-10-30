4a5e9fab452c7ef2665fe91c8066095d606e769d
Umberto Valleriani <umberto.valleriani@gmail.com>
2020-10-30 09:10:52 +0100

    Formatting everything

--- templates/HTML_template_elements.py
+++ templates/HTML_template_elements.py
@@ -4,24 +4,34 @@ Templates and functions for making the results HTML file.
 SpiceBucks
 """
 
-HEADER = '''<!DOCTYPE html>
+HEADER = """<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Arb Time</title>
 </head>
 <body>
-<img src="templates/police.jpg" alt="Don't">'''
+<img src="templates/police.jpg" alt="Don't">"""
 
-FOOTER = '''</body>
-</html>'''
+FOOTER = """</body>
+</html>"""
+
 
 def make_div(result):
-    ret = "<h1>" + result["Name"] + "</h1> <h2> " + result["Arbitrage Opportunity"] + "</h2> "
-    ret += '<h3> <a href="' + result["Link"] + '"> ' + result["Link"] + '</a> </h3> <br />'
+    ret = (
+        "<h1>"
+        + result["Name"]
+        + "</h1> <h2> "
+        + result["Arbitrage Opportunity"]
+        + "</h2> "
+    )
+    ret += (
+        '<h3> <a href="' + result["Link"] + '"> ' + result["Link"] + "</a> </h3> <br />"
+    )
     for r in result["Instructions"]:
         ret += "<span> " + r + "<br /> </span> "
     return ret
+
 
 def make_html(res_list):
     ret = HEADER
--- util/message.py
+++ util/message.py
@@ -1,9 +1,9 @@
-'''
+"""
 Contains a class for messages to be used within the framework. Make sure to include "message" and
 not "CMessage". This acts as a static class.
 
 SpiceBucks
-'''
+"""
 
 # ------------------------------------------------------------------
 
@@ -14,11 +14,13 @@ RESULT_TAG = "RESULT"
 
 # ------------------------------------------------------------------
 
+
 class CMessage(object):
-    '''
+    """
     A class for printing terminal messages and saving them to a log file.
-    '''
-    def __init__(self, logfile='log.txt'):
+    """
+
+    def __init__(self, logfile="log.txt"):
         self._logname = logfile
         # start the logfile
         self._logfile = None
@@ -30,53 +32,53 @@ class CMessage(object):
     # 'public' members
     # ------------------------------------------------------------------
 
-    def logError(self, message, funcname=''):
-        '''
+    def logError(self, message, funcname=""):
+        """
         Log a message with the 'ERROR' tag.
 
         :param str message: Is the text of the message to be dispalyed.
         :param str funcname: Is an optional argument that is used for printing the name of \
         the function where the message is coming from.
-        '''
+        """
         self._log(message, ERROR_TAG, funcname)
 
-    def logWarning(self, message, funcname=''):
-        '''
+    def logWarning(self, message, funcname=""):
+        """
         Log a message with the 'WARNING' tag.
 
         :param str message: Is the text of the message to be dispalyed.
         :param str funcname: Is an optional argument that is used for printing the name of \
         the function where the message is coming from.
-        '''
+        """
         self._log(message, WARNING_TAG, funcname)
 
-    def logDebug(self, message, funcname=''):
-        '''
+    def logDebug(self, message, funcname=""):
+        """
         Log a message with the 'DEBUG' tag.
 
         :param str message: Is the text of the message to be dispalyed.
         :param str funcname: Is an optional argument that is used for printing the name of \
         the function where the message is coming from.
-        '''
+        """
         self._log(message, DEBUG_TAG, funcname)
 
-    def logResult(self, message, funcname=''):
-        '''
+    def logResult(self, message, funcname=""):
+        """
         Log a message with the 'DEBUG' tag.
 
         :param str message: Is the text of the message to be dispalyed.
         :param str funcname: Is an optional argument that is used for printing the name of \
         the function where the message is coming from.
-        '''
+        """
         self._log(message, RESULT_TAG, funcname)
 
     # ------------------------------------------------------------------
     # 'private' members
     # ------------------------------------------------------------------
 
-    def _log(self, msg, level, funcname=''):
+    def _log(self, msg, level, funcname=""):
         # format message and send it to console
-        if funcname != '':
+        if funcname != "":
             message = "{0}::{1}::{2}".format(level, funcname, msg)
             print(message)
         else:
@@ -84,17 +86,17 @@ class CMessage(object):
             print(message)
         # save it to the log file
         if self._logfile is not None:
-            self._logfile.write((message + '\n').encode('utf-8'))
+            self._logfile.write((message + "\n").encode("utf-8"))
 
     def _logFile(self, newname):
-        '''
+        """
         Make a new log file at the given location
         :param newname: A string of the path and name to the new log file
-        '''
+        """
         if self._logfile is not None:
             self._logfile.close()
         self._logname = newname
-        self._logfile = open(newname, 'wb')
+        self._logfile = open(newname, "wb")
 
 
 message = CMessage()
