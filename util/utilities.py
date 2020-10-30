2ac615bd8012aca6a561e218fbbe6bd2274a025b
Umberto Valleriani <umberto.valleriani@gmail.com>
2020-10-30 09:17:44 +0100

    Formatting with Black

--- util/utilities.py
+++ util/utilities.py
@@ -1,10 +1,10 @@
-'''
+"""
 Utility functions go here.
 
 SpiceBucks
-'''
+"""
 
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 
 import sys
 import pygame
@@ -12,30 +12,35 @@ import numpy as np
 
 from util.message import message
 
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # system functions
-#----------------------------------------z-------------------------------------------------
+# ----------------------------------------z-------------------------------------------------
+
 
 def exit(code):
-    '''
-	Exit the program, 0 is failure, 1 is success.
-	'''
+    """
+    Exit the program, 0 is failure, 1 is success.
+    """
     if not isinstance(code, int):
-        message.logError('Exit code must be an interger.')
+        message.logError("Exit code must be an interger.")
         exit(0)
     if code == 0:
-        message.logError('Exiting program with failure status.')
+        message.logError("Exiting program with failure status.")
     elif code == 1:
-        message.logDebug('Exiting program with success status.')
+        message.logDebug("Exiting program with success status.")
     else:
-        message.logError('Exiting program with unknown error status ('+str(code)+')')
+        message.logError(
+            "Exiting program with unknown error status (" + str(code) + ")"
+        )
     sys.exit()
 
-#-----------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------
 # sound functions
-#----------------------------------------z-------------------------------------------------
+# ----------------------------------------z-------------------------------------------------
+
 
 def beep(file_name):
     pygame.mixer.init()
